a4768b96ec8463aa26533e6e1843411e34cbb7e2
Niklas Hegnelt <wispster@gmail.com>
2021-06-06 12:30:23 +0200

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgxGaugeModule } from 'ngx-gauge';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgxGaugeModule],
+  imports: [BrowserModule, NgxGaugeModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
